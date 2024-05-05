24b5a19484b8f9ec635958a5802ee87a6e9fd09a
fanuelhpa <fanuelrique@gmail.com>
2024-05-05 16:25:34 +1200

    Obtendo um usuário por id e tratamento de exceção

---
+++ src/main/java/com/fandev/workshopmongo/services/exceptions/ResouceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.fandev.workshopmongo.services.exceptions;
+
+public class ResouceNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public ResouceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+}
